6448cf7c1413f8f7a4e9c67b1a6a8595eb4c9d2f
Sangryu Park <psrpsj@gmail.com>
2022-07-25 18:41:47 +0000

    add code in train.py

--- train.py
+++ train.py
@@ -1,11 +1,29 @@
+from arguments import TrainingArguments
+
 from transformers import (
+    AutoConfig,
     AutoTokenizer,
     AutoModelForSequenceClassification,
     HfArgumentParser,
-    TrainingArguments
+    TrainingArguments,
+    set_seed,
 )
 
+
 def main():
+    model_name = "klue/bert-base"
+    data_path = "./dataset/train.csv"
     parser = HfArgumentParser(TrainingArguments)
     training_args = parser.parse_args_into_dataclasses()
-    
+
+    print(f"Current Model is {model_name}")
+
+    set_seed(training_args.seed)
+
+    tokenizer = AutoTokenizer.from_pretrained(pretrained_model_name_or_path=model_name)
+    model_config = AutoConfig.from_pretrained(pretrained_model_name_or_path=model_name)
+    model_config.num_labels = 4
+    model = AutoModelForSequenceClassification.from_pretrained(
+        model_name, config=model_config
+    )
+    model.train()
