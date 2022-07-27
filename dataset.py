07972235bc34524ec317a4ea79e8b3a3b73e6c57
Sangryu Park <psrpsj@gmail.com>
2022-07-27 17:07:23 +0000

    add code in dataset.py

--- dataset.py
+++ dataset.py
@@ -1,7 +1,10 @@
+import pandas as pd
+
 from datasets import load_dataset
 
 
-def load_data(path):
-    dataset = load_dataset("csv", data_files=path)
-
+def create_token_data(path):
+    dataset = pd.read_csv(path)
     review = ["[CLS] " + str(r) + " [SEP]" for r in dataset["reviews"]]
+    dataset["reviews"] = review
+    dataset.to_csv("./dataset/train_w_token.csv")
