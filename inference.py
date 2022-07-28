b4f69515da42e23622928f22046f7993622561a1
Sangryu Park <psrpsj@gmail.com>
2022-07-28 19:12:47 +0000

    fix typo in inference

--- inference.py
+++ inference.py
@@ -9,4 +9,5 @@ def main(args):
     data_path = "./dataset/test.csv"
     tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-    test_dataset = pd.read_csv(data_path)
+    dataset = pd.read_csv(data_path)
+    dataset["label"] = [100] * len(dataset)
