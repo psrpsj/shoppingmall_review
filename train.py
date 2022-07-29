489097744abb0f11eb15c6c5fb4a3b9edcfc9cc6
Sangryu Park <psrpsj@gmail.com>
2022-07-29 19:04:10 +0000

    add kfold process in train

--- train.py
+++ train.py
@@ -1,11 +1,12 @@
 import argparse
+import os
 import pandas as pd
 import torch
 import wandb
 
 from arguments import TrainingArguments
 from dataset import CustomDataset
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StartifiedKFold, train_test_split
 from sklearn.metrics import accuracy_score
 from transformers import (
     AutoConfig,
@@ -38,44 +39,106 @@ def main(args):
 
     set_seed(training_args.seed)
     total_dataset = pd.read_csv(data_path)
+    total_label = total_dataset["target"]
     tokenizer = AutoTokenizer.from_pretrained(pretrained_model_name_or_path=model_name)
 
+    # K-Fold Process
+    if args.k_fold:
+        print("---- STARTING K-FOLD ----")
+        fold_num = 1
+        k_fold = StartifiedKFold(n_split=5, shuffle=False)
+        for train_index, valid_index in k_fold.split(total_dataset, total_label):
+            wandb.init(
+                entity="psrpsj",
+                project="shoppingmall",
+                name=args.project_name + "_kfold_" + str(fold_num),
+                tags=args.model_name,
+            )
+            wandb.config.update(training_args)
+
+            print(f"---- Fold Number {fold_num} start ----")
+            output_dir = os.path.join(
+                training_args.output_dir, "kfold", "fold" + str(fold_num)
+            )
+
+            train_dataset, valid_dataset = (
+                total_dataset.iloc[train_index],
+                total_dataset.iloc[valid_index],
+            )
+
+            train_label, valid_label = (
+                total_label.iloc[train_index],
+                total_label.iloc[valid_index],
+            )
+
+            train = CustomDataset(train_dataset, train_label, tokenizer)
+            valid = CustomDataset(valid_dataset, valid_label, tokenizer)
+
+            model_config = AutoConfig.from_pretrained(
+                pretrained_model_name_or_path=args.model_name
+            )
+            model_config.num_labels = 6
+            model = AutoModelForSequenceClassification(
+                args.model_name, config=model_config
+            )
+            model.resize_token_embeddings(len(tokenizer))
+            model.to(device)
+            model.train()
+
+            trainer = Trainer(
+                model=model,
+                args=training_args,
+                train_dataset=train,
+                eval_dataset=valid,
+                compute_metrics=compute_metrics,
+            )
+            trainer.train()
+            model.save_pretrained(output_dir)
+            fold_idx += 1
+
     # Non K-Fold Process
-    model_config = AutoConfig.from_pretrained(pretrained_model_name_or_path=model_name)
-    model_config.num_labels = 6
-    model = AutoModelForSequenceClassification.from_pretrained(
-        model_name, config=model_config
-    )
-    model.resize_token_embeddings(len(tokenizer))
-    model.to(device)
-    model.train()
+    else:
+        model_config = AutoConfig.from_pretrained(
+            pretrained_model_name_or_path=args.model_name
+        )
+        model_config.num_labels = 6
+        model = AutoModelForSequenceClassification.from_pretrained(
+            args.model_name, config=model_config
+        )
+        model.resize_token_embeddings(len(tokenizer))
+        model.to(device)
+        model.train()
 
-    wandb.init(
-        entity="psrpsj",
-        project="shoppingmall",
-        name=args.project_name,
-        tags=model_name,
-    )
+        wandb.init(
+            entity="psrpsj",
+            project="shoppingmall",
+            name=args.project_name,
+            tags=args.model_name,
+        )
 
-    wandb.config.update(training_args)
+        wandb.config.update(training_args)
 
-    train_dataset, valid_dataset = train_test_split(
-        total_dataset, test_size=0.2, stratify=total_dataset["target"], random_state=42
-    )
-    train = CustomDataset(train_dataset, train_dataset["target"].tolist(), tokenizer)
-    valid = CustomDataset(valid_dataset, valid_dataset["target"].tolist(), tokenizer)
+        train_dataset, valid_dataset = train_test_split(
+            total_dataset, test_size=0.2, stratify=total_label, random_state=42
+        )
+        train = CustomDataset(
+            train_dataset, train_dataset["target"].tolist(), tokenizer
+        )
+        valid = CustomDataset(
+            valid_dataset, valid_dataset["target"].tolist(), tokenizer
+        )
 
-    trainer = Trainer(
-        model=model,
-        args=training_args,
-        train_dataset=train,
-        eval_dataset=valid,
-        compute_metrics=compute_metrics,
-    )
+        trainer = Trainer(
+            model=model,
+            args=training_args,
+            train_dataset=train,
+            eval_dataset=valid,
+            compute_metrics=compute_metrics,
+        )
 
-    print("---- START TRAINING ----")
-    trainer.train()
-    model.save_pretrained(training_args.output_dir + args.project_name)
+        print("---- START TRAINING ----")
+        trainer.train()
+        model.save_pretrained(training_args.output_dir + args.project_name)
     print("---- FINISH ----")
 
 
