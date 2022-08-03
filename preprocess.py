2372fce9401ae86126356473d1198917d8b947e9
Sangryu Park <psrpsj@gmail.com>
2022-08-03 17:46:47 +0000

    preprocess modified

--- preprocess.py
+++ preprocess.py
@@ -7,13 +7,12 @@ from tqdm import tqdm
 
 
 def preprocess(data_path, file_name):
-    file_name = file_name.replace(".csv", "")
     if os.path.exists(os.path.join(data_path, file_name + "_fix.csv")):
         print("Preprocess file already exist!")
         return pd.read_csv(os.path.join(data_path, file_name + "_fix.csv"))
 
     else:
-        before = pd.read_csv(os.path.join(data_path, file_name + ".csv"))
+        before = pd.read_csv(os.path.join(data_path, file_name))
         after_id, after_review, after_target = [], [], []
         print("---- START PREPROCESSING ----")
         for idx in tqdm(range(len(before))):
@@ -25,24 +24,15 @@ def preprocess(data_path, file_name):
             after_id.append(before["id"].iloc[idx])
             after_review.append(spell_checked.checked)
             if "train" in file_name:
-              after_target.append(before["target"].iloc[idx])
+                after_target.append(before["target"].iloc[idx])
 
-        after = pd.DataFrame()
-        if "train" in file_name:
-          after = pd.DataFrame(
-              {
-                  "id": after_id,
-                  "reviews": after_review,
-                  "target": after_target,
-              }
-          )
-        else:
-          after = pd.DataFrame(
-              {
-                  "id": after_id,
-                  "reviews": after_review,
-              }
-          )
+        after = pd.DataFrame(
+            {
+                "id": after_id,
+                "reviews": after_review,
+                "target": after_target,
+            }
+        )
         after.to_csv(os.path.join(data_path, file_name + "_fix.csv"), index=False)
         print("---- FINISH PREPROCESSING ----")
         return after
